581f1ad476f5e6aae7ec119180e61d53a737f0b4
Vitor Leal dos Santos <vitor_leal2201@hotmail.com>
2017-09-06 00:49:30 -0300

    Criação da classe Blinker

---
+++ blinker.cpp
@@ -0,0 +1,15 @@
+#include "block.hpp"
+
+void Block::setPosicao(){
+  cria_Matriz();
+
+   matriz[4][4] = 'o';
+   matriz[4][5] = 'o';
+   matriz[4][6] = 'o';
+
+
+   matriz[10][10] = 'o';
+   matriz[10][11] = 'o';
+   matriz[10][12] = 'o';
+
+}
---
+++ blinker.hpp
@@ -0,0 +1,12 @@
+#include "matriz.hpp"
+
+class Blinker:public Matriz{
+
+  private:
+    int quantidade_blinker;
+  public:
+    Blinker();
+
+    void setPosicao();
+
+}
