2dedf401fc6bb7c9302ab3b4b708f0f93c7f3b54
Vitor Leal dos Santos <vitor_leal2201@hotmail.com>
2017-09-10 00:04:22 -0300

    Correcao da verificacao das regras e separacao das funcoes de verificacao

--- matriz.cpp
+++ matriz.cpp
@@ -3,9 +3,10 @@
 
 Matriz::Matriz(){
    matriz[0][0];
-   quantidade_linhas=0 ;
-   quantidade_colunas=0 ;
-   geracoes = 0;
+   prox_geracao[80][80];
+   quantidade_linhas= 0 ;
+   quantidade_colunas= 0 ;
+   quantidade_geracoes = 0;
    cel_viva = 1;
    cel_morta = 0;
 }
@@ -53,41 +54,81 @@ cout << "\n";
 
   for(int i=0 ; i<linhas ; i++){
     for(int j=0; j<colunas ; j++){
-      cout << matriz[i][j] ;
+      cout << prox_geracao[i][j] ;
     }
     cout << "\n";
   }
 
 }
 
-void Matriz::verifica_regras(){
+int Matriz::conta_vizinhos(int i, int j){
+  int vizinhos = 0;
+
+  if (matriz[i-1][j-1] == 'o')
+    vizinhos++;
+  if (matriz[i-1][j] == 'o')
+    vizinhos++;
+  if (matriz[i-1][j+1] == 'o')
+    vizinhos++;
+  if (matriz[i][j-1] == 'o')
+    vizinhos++;
+  if (matriz[i][j+1] == 'o')
+    vizinhos++;
+  if (matriz[i+1][j-1] == 'o')
+    vizinhos++;
+  if (matriz[i+1][j] == 'o')
+    vizinhos++;
+  if (matriz[i+1][j+1] == 'o')
+    vizinhos++;
+
+    return vizinhos;
+}
+
+void Matriz::inserir_celula(int i , int j , char estado){
+
+  if(estado == 'o')
+    prox_geracao[i][j] = 'o';
+  else
+    prox_geracao[i][j] = '-';
+
+}
+
+
+void Matriz::nova_geracao(){
   int linhas = getLinhas();
   int colunas = getColunas();
-  int vizinhos =0;
+  int i=0;
+  int j=0;
 
-  for(int i=0;i<linhas;i++){
-    for(int j=0;j<colunas;j++){
-      for(int k=0;k<3;k++){
-        for(int l=0; l<3; l++){
-          if(matriz[i+k][j+l] == 'o'){
-            vizinhos++;
-          }
-        }
+  for(i=0;i<linhas;i++){
+    for(j=0;j<colunas;j++){
+      if(matriz[i][j] == 'o'){
+        if (conta_vizinhos(i,j) < 2)
+          inserir_celula(i,j,'-');
+      else if (conta_vizinhos(i,j) > 3)
+          inserir_celula(i,j,'-');
+      else
+          inserir_celula(i,j,'o');
       }
-        if(vizinhos < 2){
-          matriz[i][j] = '-';
-        }
-        if(vizinhos > 3){
-          matriz[i][j] = '-';
-        }
-        if(vizinhos == 3 && matriz[i][j] == '-' ){
-          matriz[i][j] = 'o';
-        }
-        if((vizinhos == 2 || vizinhos == 3) && matriz[i][j] == 'o'){
-          matriz[i][j] = 'o';
-        }
-        vizinhos =0;
+      else{
+        if (conta_vizinhos(i,j) == 3)
+          inserir_celula(i,j,'o');
+          else
+          inserir_celula(i,j,'-');
+      }
     }
   }
 
+  copia_matriz(prox_geracao,matriz);
+
+}
+
+void Matriz::copia_matriz(char matriz1[80][80], char matriz2[80][80]){
+  int i,j = 0;
+
+  for(i=0;i<80;i++){
+    for(j=0;j<80;j++){
+      matriz2[i][j] = matriz1[i][j];
+    }
+  }
 }
--- matriz.hpp
+++ matriz.hpp
@@ -2,12 +2,14 @@
 #define MATRIZ_HPP
 
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 class Matriz{
 
   private:
     char matriz[80][80];
+    char prox_geracao[80][80];
     int quantidade_linhas;
     int quantidade_colunas;
     int geracoes;
@@ -19,15 +21,19 @@ class Matriz{
     Matriz();
 
     char jogo_desejado;
+    int quantidade_geracoes;
+
+
     int escolher_forma();
     void insere_Linhas();
     int  getLinhas();
     void insere_Colunas();
     int getColunas();
     void cria_Matriz();
-    void cria_Matriz(int quantidade_linhas, int quantidade_colunas);
-    void verifica_regras();
-    void atualiza_matriz();
+    int conta_vizinhos(int i , int j);
+    void inserir_celula(int i , int j , char estado);
+    void nova_geracao();
+    void copia_matriz(char matriz1[80][80], char matriz2[80][80]);
     void imprime_matriz();
 
 };
