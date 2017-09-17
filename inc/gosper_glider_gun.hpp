0da0ad7a0af0a35c3038f85670fca603f0fcdd2f
Vitor Leal dos Santos <vitor_leal2201@hotmail.com>
2017-09-17 00:47:12 -0300

    Criação da classe Gosper Glider Gun

---
+++ inc/gosper_glider_gun.hpp
@@ -0,0 +1,9 @@
+#include "matriz.hpp"
+
+class Gosper_glider:public Matriz{
+
+  private:
+  public:
+    Gosper_glider();
+    ~Gosper_glider();
+}
---
+++ src/gosper_glider_gun.cpp
@@ -0,0 +1,11 @@
+#include "block.hpp"
+#include "matriz.hpp"
+
+
+Gosper_glider::Gosper_glider(){
+
+}
+
+~Gosper_glider::~Gosper_glider(){
+
+}
